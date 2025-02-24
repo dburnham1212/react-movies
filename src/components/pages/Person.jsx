1b5334e73169390d4dde3c66a0e4d8fe7ac2a90f
Tania <taniaws06@gmail.com>
2025-02-24 16:25:09 -0500

    Adding My Name

--- src/components/pages/Person.jsx
+++ src/components/pages/Person.jsx
@@ -6,24 +6,26 @@ import { BASE_IMAGE_URL, BASE_URL } from "../../constants/constants";
 import styles from "../../styles/pages/Person.module.css";
 
 const Person = () => {
-    const [personData, setPersonData] = useState({});
+  const [personData, setPersonData] = useState({});
 
-    const { id } = useParams();
+  const { id } = useParams();
 
-    useEffect(() => {
-        // Dylan Burnham
-        makeApiCall(`${BASE_URL}/person/${id}?api_key=${process.env.REACT_APP_API_KEY}`).then((response) => {
-            console.log(response);
-            setPersonData(response);
-        });
-    }, []);
+  useEffect(() => {
+    // Tania H
+    makeApiCall(
+      `${BASE_URL}/person/${id}?api_key=${process.env.REACT_APP_API_KEY}`
+    ).then((response) => {
+      console.log(response);
+      setPersonData(response);
+    });
+  }, []);
 
-    return (
-        <>
-            <h1 className={styles.title}>{personData.name}</h1>
-            <img src={`${BASE_IMAGE_URL}${personData.profile_path}`} />
-        </>
-    );
+  return (
+    <>
+      <h1 className={styles.title}>{personData.name}</h1>
+      <img src={`${BASE_IMAGE_URL}${personData.profile_path}`} />
+    </>
+  );
 };
 
 export default Person;
