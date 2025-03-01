ff2f1098320136e5b000a1c78fbf866bd4f55a35
Dylan Burnham <dburnham1212@gmail.com>
2025-02-28 21:04:04 -0500

    Adding pagination to search

--- src/components/pages/Search.jsx
+++ src/components/pages/Search.jsx
@@ -1,18 +1,17 @@
 import {
-    Autocomplete,
-    Box,
     Divider,
     FormControl,
     IconButton,
     InputBase,
     InputLabel,
     MenuItem,
+    Pagination,
     Paper,
     Select,
-    TextField,
+    Stack,
 } from "@mui/material";
 import styles from "../../styles/pages/Search.module.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { makeApiCall } from "../../helper/helperFunctions";
 import { BASE_URL } from "../../constants/constants";
 import MediaCard from "../utility/Cards/MediaCard";
@@ -23,71 +22,73 @@ const Search = () => {
     const [searchFilter, setSearchFilter] = useState("All");
     const [searchData, setSearchData] = useState([]);
     const [hasSearched, setHasSearched] = useState(false);
-    const [availableLanguages, setAvailableLanguages] = useState({});
+    const [totalPages, setTotalPages] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
 
-    useEffect(() => {
-        makeApiCall(
-            `${BASE_URL}/configuration/languages?api_key=${process.env.REACT_APP_API_KEY}&query=${searchTerm}`
-        ).then((response) => {
-            console.log(response);
-            setAvailableLanguages(response.sort((a, b) => a.english_name.localeCompare(b.english_name)));
-        });
-    }, []);
-
-    const searchUsingTerm = () => {
+    const searchUsingTerm = (pageNumber) => {
         setHasSearched(true);
         if (searchFilter === "All") {
-            makeApiCall(`${BASE_URL}/search/multi?api_key=${process.env.REACT_APP_API_KEY}&query=${searchTerm}`).then(
-                (response) => {
-                    setSearchData(response.results);
-                    console.log(response.results);
-                }
-            );
+            makeApiCall(
+                `${BASE_URL}/search/multi?api_key=${process.env.REACT_APP_API_KEY}&query=${searchTerm}&page=${pageNumber}`
+            ).then((response) => {
+                setSearchData(response.results);
+                setTotalPages(response.total_pages);
+            });
         } else if (searchFilter === "Movies") {
-            makeApiCall(`${BASE_URL}/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${searchTerm}`).then(
-                (response) => {
-                    setSearchData(
-                        response.results.map((item) => {
-                            return { ...item, media_type: "movie" };
-                        })
-                    );
-                    console.log(response.results);
-                }
-            );
+            makeApiCall(
+                `${BASE_URL}/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${searchTerm}&page=${pageNumber}`
+            ).then((response) => {
+                setSearchData(
+                    response.results.map((item) => {
+                        return { ...item, media_type: "movie" };
+                    })
+                );
+                setTotalPages(response.total_pages);
+            });
         } else if (searchFilter === "TV") {
-            makeApiCall(`${BASE_URL}/search/tv?api_key=${process.env.REACT_APP_API_KEY}&query=${searchTerm}`).then(
-                (response) => {
-                    setSearchData(
-                        response.results.map((item) => {
-                            return { ...item, media_type: "tv" };
-                        })
-                    );
-                    console.log(response.results);
-                }
-            );
+            makeApiCall(
+                `${BASE_URL}/search/tv?api_key=${process.env.REACT_APP_API_KEY}&query=${searchTerm}&page=${pageNumber}`
+            ).then((response) => {
+                setSearchData(
+                    response.results.map((item) => {
+                        return { ...item, media_type: "tv" };
+                    })
+                );
+                setTotalPages(response.total_pages);
+            });
         } else {
-            makeApiCall(`${BASE_URL}/search/person?api_key=${process.env.REACT_APP_API_KEY}&query=${searchTerm}`).then(
-                (response) => {
-                    setSearchData(
-                        response.results.map((item) => {
-                            return { ...item, media_type: "person" };
-                        })
-                    );
-                    console.log(response.results);
-                }
-            );
+            makeApiCall(
+                `${BASE_URL}/search/person?api_key=${process.env.REACT_APP_API_KEY}&query=${searchTerm}&page=${pageNumber}`
+            ).then((response) => {
+                setSearchData(
+                    response.results.map((item) => {
+                        return { ...item, media_type: "person" };
+                    })
+                );
+                setTotalPages(response.total_pages);
+            });
         }
+    };
+
+    const searchWithPageNumber = (value) => {
+        searchUsingTerm(value);
+        setCurrentPage(value);
+    };
+
+    const handlePageChange = (e, value) => {
+        setCurrentPage(value);
+        searchUsingTerm(value);
     };
 
     const handleKeypress = (e) => {
         if (e.keyCode === 13) {
-            searchUsingTerm();
+            setCurrentPage(1);
+            searchUsingTerm(1);
         }
     };
 
     const handleSearchTermChange = (e) => {
         setSearchTerm(e.target.value);
-        console.log(e.target.value);
     };
 
     const handleSearchFilterChange = (e) => {
@@ -131,7 +132,7 @@ const Search = () => {
                     />
                     <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
                     <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
-                        <SearchIcon onClick={() => searchUsingTerm()} />
+                        <SearchIcon onClick={() => searchWithPageNumber(1)} />
                     </IconButton>
                 </Paper>
             </div>
@@ -154,6 +155,21 @@ const Search = () => {
                         return <MediaCard key={index} media={searchItem} />;
                     })}
             </div>
+            {totalPages > 1 && (
+                <div className={styles.pagination_box}>
+                    <Stack spacing={2}>
+                        <Pagination
+                            count={totalPages}
+                            page={currentPage}
+                            onChange={handlePageChange}
+                            variant="outlined"
+                            shape="rounded"
+                            showFirstButton
+                            showLastButton
+                        />
+                    </Stack>
+                </div>
+            )}
         </div>
     );
 };
