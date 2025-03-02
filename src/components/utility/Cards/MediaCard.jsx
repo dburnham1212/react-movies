57ec12292d3697136df41ce614db4b64b6efe31e
Dylan Burnham <dburnham1212@gmail.com>
2025-03-01 21:55:20 -0500

    Filling out the home page

--- src/components/utility/Cards/MediaCard.jsx
+++ src/components/utility/Cards/MediaCard.jsx
@@ -4,11 +4,18 @@ import { Card } from "@mui/material";
 import StarIcon from "@mui/icons-material/Star";
 
 const MediaCard = (props) => {
-    const { media } = props;
+    const { media, displayType, mediaType } = props;
     return (
         <>
-            <Card sx={{ padding: ".5rem", display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
-                <a href={`/${media.media_type}/${media.id}`}>
+            <Card
+                sx={{
+                    padding: ".5rem",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                }}
+            >
+                <a href={`/${media.media_type || mediaType}/${media.id}`}>
                     <img
                         className={styles.main_image}
                         src={`${
@@ -20,24 +27,31 @@ const MediaCard = (props) => {
                     />
                 </a>
                 <div className={styles.lower_container}>
-                    {media.media_type === "person" ? (
-                        <>
-                            <h4 className={styles.title_text}>{`(${
-                                media.media_type.slice(0, 1).toUpperCase() + media.media_type.slice(1)
-                            })`}</h4>
-                        </>
-                    ) : (
-                        <>
-                            <div className={styles.rating_type_box}>
+                    {displayType ? (
+                        media.media_type === "person" ? (
+                            <>
                                 <h4 className={styles.title_text}>{`(${
                                     media.media_type.slice(0, 1).toUpperCase() + media.media_type.slice(1)
                                 })`}</h4>
-                                <div className={styles.title_text}>
-                                    <StarIcon sx={{ fontSize: "medium", color: "#fea423" }} />
-                                    <span>{Number(media.vote_average / 2).toFixed(1)}</span>
+                            </>
+                        ) : (
+                            <>
+                                <div className={styles.rating_type_box}>
+                                    <h4 className={styles.title_text}>{`(${
+                                        media.media_type.slice(0, 1).toUpperCase() + media.media_type.slice(1)
+                                    })`}</h4>
+                                    <div className={styles.title_text}>
+                                        <StarIcon sx={{ fontSize: "medium", color: "#fea423" }} />
+                                        <span>{Number(media.vote_average / 2).toFixed(1)}</span>
+                                    </div>
                                 </div>
-                            </div>
-                        </>
+                            </>
+                        )
+                    ) : (
+                        <div className={styles.title_text}>
+                            <StarIcon sx={{ fontSize: "medium", color: "#fea423" }} />
+                            <span>{Number(media.vote_average / 2).toFixed(1)}</span>
+                        </div>
                     )}
                     <h4 className={styles.title_text}>{`${media.title || media.name}`}</h4>
                 </div>
