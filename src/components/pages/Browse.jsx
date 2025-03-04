2adbf608f726460722aaeb736581083bfb04ee34
Dylan Burnham <dburnham1212@gmail.com>
2025-03-03 23:59:30 -0500

    Working on proper browse page

--- src/components/pages/Browse.jsx
+++ src/components/pages/Browse.jsx
@@ -20,11 +20,12 @@ import {
 } from "../../helper/helperFunctions";
 
 // import stuff from MUI (Material UI)
-import { FormControl, Select, MenuItem, InputLabel } from "@mui/material";
+import { FormControl, Select, MenuItem, InputLabel, Stack, Pagination } from "@mui/material";
 
 import styles from "../../styles/pages/Browse.module.css";
 
 import MediaImageRow from "../utility/ImageRows/MediaImageRow";
+import MediaCard from "../utility/Cards/MediaCard";
 
 const Browse = () => {
     // This is an example of the useState hook, think of it like a declaration and a setter function
@@ -33,9 +34,21 @@ const Browse = () => {
     // The trendingMovies variable would then be initialized to an empty array []
     const [mediaType, setMediaType] = useState("Movies");
 
-    const [actionMovies, setActionMovies] = useState([]);
-    const [comedyMovies, setComedyMovies] = useState([]);
-    const [adventureMovies, setAdventureMovies] = useState([]);
+    // Movie genre states
+    const [movieGenres, setMovieGenres] = useState([]);
+    const [movieGenre, setMovieGenre] = useState({});
+
+    // Movie Info
+    const [selectedGenreMovies, setSelectedGenreMovies] = useState({});
+
+    // TV genre states
+    const [tvGenres, setTvGenres] = useState([]);
+    const [tvGenre, setTvGenre] = useState({});
+
+    // Movie Info
+    const [selectedGenreTv, setSelectedGenreTv] = useState({});
+
+    const [currentPage, setCurrentPage] = useState(1);
 
     const [animationTvShows, setAnimationTVShows] = useState([]);
     const [comedyTVShows, setComedyTVShows] = useState([]);
@@ -47,6 +60,24 @@ const Browse = () => {
 
     const [strangerThingsOneEpisodes, setStrangerThingsOneEpisodes] = useState([]);
 
+    const getMoviesForGenre = (genreId, currentPage) => {
+        makeApiCall(
+            `${BASE_URL}/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=${genreId}&page=${currentPage}`
+        ).then((response) => {
+            console.log(response);
+            setSelectedGenreMovies(response);
+        });
+    };
+
+    const getTvForGenre = (genreId, currentPage) => {
+        makeApiCall(
+            `${BASE_URL}/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&with_genres=${genreId}&page=${currentPage}`
+        ).then((response) => {
+            console.log(response);
+            setSelectedGenreTv(response);
+        });
+    };
+
     useEffect(() => {
         // This is an example of the use effect hook!
         // We can use the use effect hook to create api calls
@@ -56,26 +87,18 @@ const Browse = () => {
         makeApiCall(`${BASE_URL}/genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}`).then((response) => {
             console.log("===== Movie Genre List =====");
             console.log(response.genres);
+            setMovieGenres(response.genres);
+            setMovieGenre(response.genres[0]);
+            getMoviesForGenre(response.genres[0].id, currentPage);
         });
 
         makeApiCall(`${BASE_URL}/genre/tv/list?api_key=${process.env.REACT_APP_API_KEY}`).then((response) => {
-            console.log(response);
             console.log("===== TV Genre List =====");
-            console.log(response.genres);
+            console.log(response);
+            setTvGenres(response.genres);
+            setTvGenre(response.genres[0]);
+            getTvForGenre(response.genres[0].id, currentPage);
         });
-
-        // Movie API Calls
-        makeApiCall(
-            `${BASE_URL}/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=${ACTION_MOVIE_VAL}`
-        ).then((response) => setActionMovies(refineMovies(response.results)));
-
-        makeApiCall(
-            `${BASE_URL}/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=${COMEDY_MOVIE_VAL}`
-        ).then((response) => setComedyMovies(refineMovies(response.results)));
-
-        makeApiCall(
-            `${BASE_URL}/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=${ADVENTURE_MOVIE_VAL}`
-        ).then((response) => setAdventureMovies(refineMovies(response.results)));
 
         // TV API Calls
         makeApiCall(
@@ -109,91 +132,190 @@ const Browse = () => {
 
     const handleMediaTypeChange = (e) => {
         setMediaType(e.target.value);
+        if (e.target.value === "Movies") {
+            // reset the movie search to the first selection
+            setMovieGenre(movieGenres[0]);
+            getMoviesForGenre(movieGenres[0].id, 1);
+        } else if (e.target.value === "TV") {
+            // reset the tv search to the first selection
+            setTvGenre(tvGenres[0]);
+            getTvForGenre(tvGenres[0].id, 1);
+        }
+        setCurrentPage(1);
+    };
+
+    const handleMovieGenreChange = (e) => {
+        setMovieGenre(movieGenres.find((genre) => e.target.value === genre.id));
+        setCurrentPage(1);
+        getMoviesForGenre(e.target.value, 1);
+    };
+
+    const handleTvGenreChange = (e) => {
+        setTvGenre(tvGenres.find((genre) => e.target.value === genre.id));
+        setCurrentPage(1);
+        getTvForGenre(e.target.value, 1);
+    };
+
+    const handleMoviePageChange = (e, value) => {
+        setCurrentPage(value);
+        getMoviesForGenre(movieGenre.id, value);
+    };
+
+    const handleTvPageChange = (e, value) => {
+        setCurrentPage(value);
+        getTvForGenre(tvGenre.id, value);
     };
 
     return (
         <>
             <div className="wrapper">
-                <h1 className={styles.title}>Browse</h1>
+                <div className={styles.page_container}>
+                    <FormControl sx={{ marginRight: 2.5, minWidth: "25%" }}>
+                        <InputLabel id="select-media-label">Media Type</InputLabel>
+                        <Select
+                            labelId="select-media-label"
+                            id="select-media"
+                            value={mediaType}
+                            label="Media Type"
+                            onChange={handleMediaTypeChange}
+                        >
+                            <MenuItem value={"Movies"}>Movies</MenuItem>
+                            <MenuItem value={"TV"}>TV</MenuItem>
+                            <MenuItem value={"People"}>People</MenuItem>
+                            <MenuItem value={"Seasons"}>Seasons</MenuItem>
+                            <MenuItem value={"Episodes"}>Episodes</MenuItem>
+                        </Select>
+                    </FormControl>
+                    {/* Movies browse info */}
+                    {movieGenres?.length && mediaType === "Movies" && (
+                        <FormControl sx={{ minWidth: "25%" }}>
+                            <InputLabel id="select-media-label">Genre</InputLabel>
+                            <Select
+                                labelId="select-media-label"
+                                id="select-media"
+                                value={movieGenre.id}
+                                label="Genre"
+                                onChange={handleMovieGenreChange}
+                            >
+                                {movieGenres.map((genre, index) => (
+                                    <MenuItem key={index} value={genre.id}>
+                                        {genre.name}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    )}
 
-                <FormControl sx={{ marginLeft: 2.5, minWidth: 120 }}>
-                    <InputLabel id="select-media-label">Media Type</InputLabel>
-                    <Select
-                        labelId="select-media-label"
-                        id="select-media"
-                        value={mediaType}
-                        label="Media Type"
-                        onChange={handleMediaTypeChange}
-                    >
-                        <MenuItem value={"Movies"}>Movies</MenuItem>
-                        <MenuItem value={"TV"}>TV</MenuItem>
-                        <MenuItem value={"People"}>People</MenuItem>
-                        <MenuItem value={"Seasons"}>Seasons</MenuItem>
-                        <MenuItem value={"Episodes"}>Episodes</MenuItem>
-                    </Select>
-                </FormControl>
-
-                {mediaType === "Movies" && (
-                    <>
-                        {actionMovies.length && (
-                            <MediaImageRow title={"Action Movies"} media={actionMovies} basePath={"/movie"} />
-                        )}
-
-                        {comedyMovies.length && (
-                            <MediaImageRow title={"Comedy Movies"} media={comedyMovies} basePath={"/movie"} />
-                        )}
-
-                        {adventureMovies.length && (
-                            <MediaImageRow title={"Adventure Movies"} media={adventureMovies} basePath={"/movie"} />
-                        )}
-                    </>
-                )}
-                {mediaType === "TV" && (
-                    <>
-                        {animationTvShows.length && (
-                            <MediaImageRow title={"Animated TV"} media={animationTvShows} basePath={"/tv"} />
-                        )}
-                        {animationTvShows.length && (
-                            <MediaImageRow title={"Comedy TV"} media={comedyTVShows} basePath={"/tv"} />
-                        )}
-                        {sciFiFantasyTvShows.length && (
-                            <MediaImageRow
-                                title={"Sci-Fi and Fantasy TV"}
-                                media={sciFiFantasyTvShows}
-                                basePath={"/tv"}
-                            />
-                        )}
-                    </>
-                )}
-                {mediaType === "People" && (
-                    <>
-                        {trendingPeople.length && (
-                            <MediaImageRow title={"Trending People"} media={trendingPeople} basePath={"/person"} />
-                        )}
-                    </>
-                )}
-                {mediaType === "Seasons" && (
-                    <>
-                        {strangerThingsSeasons.length && (
-                            <MediaImageRow
-                                title={"Stranger Things"}
-                                media={strangerThingsSeasons}
-                                basePath={"/season"}
-                            />
-                        )}
-                    </>
-                )}
-                {mediaType === "Episodes" && (
-                    <>
-                        {strangerThingsOneEpisodes.length && (
-                            <MediaImageRow
-                                title={"Stranger Things One Episodes"}
-                                media={strangerThingsOneEpisodes}
-                                basePath={"/episode"}
-                            />
-                        )}
-                    </>
-                )}
+                    {mediaType === "Movies" && (
+                        <>
+                            <div className={styles.card_container}>
+                                {/* Get Movies By Selected Genre*/}
+                                {selectedGenreMovies?.results?.length &&
+                                    selectedGenreMovies?.results?.map((movie, index) => {
+                                        return (
+                                            <MediaCard
+                                                key={index}
+                                                media={movie}
+                                                displayType={false}
+                                                mediaType={"movie"}
+                                            />
+                                        );
+                                    })}
+                            </div>
+                            <div className={styles.pagination_box}>
+                                <Stack spacing={2}>
+                                    <Pagination
+                                        count={
+                                            selectedGenreMovies.total_pages > 500
+                                                ? 500
+                                                : selectedGenreMovies.total_pages
+                                        }
+                                        page={currentPage}
+                                        onChange={handleMoviePageChange}
+                                        variant="outlined"
+                                        shape="rounded"
+                                        showFirstButton
+                                        showLastButton
+                                    />
+                                </Stack>
+                            </div>
+                        </>
+                    )}
+                    {/* TV browse info */}
+                    {tvGenres?.length && mediaType === "TV" && (
+                        <FormControl sx={{ minWidth: "25%" }}>
+                            <InputLabel id="select-media-label">Genre</InputLabel>
+                            <Select
+                                labelId="select-media-label"
+                                id="select-media"
+                                value={tvGenre.id}
+                                label="Genre"
+                                onChange={handleTvGenreChange}
+                            >
+                                {tvGenres.map((genre, index) => (
+                                    <MenuItem key={index} value={genre.id}>
+                                        {genre.name}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    )}
+                    {mediaType === "TV" && (
+                        <>
+                            <div className={styles.card_container}>
+                                {/* Get Movies By Selected Genre*/}
+                                {selectedGenreTv?.results?.length &&
+                                    selectedGenreTv?.results?.map((movie, index) => {
+                                        return (
+                                            <MediaCard key={index} media={movie} displayType={false} mediaType={"tv"} />
+                                        );
+                                    })}
+                            </div>
+                            <div className={styles.pagination_box}>
+                                <Stack spacing={2}>
+                                    <Pagination
+                                        count={selectedGenreTv.total_pages > 500 ? 500 : selectedGenreTv.total_pages}
+                                        page={currentPage}
+                                        onChange={handleTvPageChange}
+                                        variant="outlined"
+                                        shape="rounded"
+                                        showFirstButton
+                                        showLastButton
+                                    />
+                                </Stack>
+                            </div>
+                        </>
+                    )}
+                    {mediaType === "People" && (
+                        <>
+                            {trendingPeople.length && (
+                                <MediaImageRow title={"Trending People"} media={trendingPeople} basePath={"/person"} />
+                            )}
+                        </>
+                    )}
+                    {mediaType === "Seasons" && (
+                        <>
+                            {strangerThingsSeasons.length && (
+                                <MediaImageRow
+                                    title={"Stranger Things"}
+                                    media={strangerThingsSeasons}
+                                    basePath={"/season"}
+                                />
+                            )}
+                        </>
+                    )}
+                    {mediaType === "Episodes" && (
+                        <>
+                            {strangerThingsOneEpisodes.length && (
+                                <MediaImageRow
+                                    title={"Stranger Things One Episodes"}
+                                    media={strangerThingsOneEpisodes}
+                                    basePath={"/episode"}
+                                />
+                            )}
+                        </>
+                    )}
+                </div>
             </div>
         </>
     );
